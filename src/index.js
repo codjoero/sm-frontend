158c79875a2bda6250d8efbd3452a6d2add61e45
codjoero <rownee2001@gmail.com>
2019-03-23 17:04:24 +0300

    164770319-chore(setup react): setup an empty react project
    - install Node
    - intialise npm
    - setup webpack and babel
    - add linting
    
    [Starts #164770319]

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+const Index = () => <h1>Store Manager</h1>;
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+import path from 'path';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.bundle.js',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+  },
+  module: {
+    rules: [
+      {
+        // this is so that we can compile any React,
+        // ES6 and above into normal ES5 syntax
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['eslint-loader'],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};
